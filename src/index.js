f44ebcb618eb2753728976040aea6e27711cccdc
William McGonagle <wpmcgonagle@gmail.com>
2022-02-05 18:45:58 -0500

    🐛 Added Commander and Colors

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 #! /usr/bin/env node
 
-console.log("Hello There");
+const { program } = require('commander');
+const colors = require('colors');
+
+program
+  .name('carrot')
+  .description('The Official CLI for the Carrot Programming Language.')
+  .version('1.0.0');
+
+program.parse();
